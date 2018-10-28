dc3f3e2d0ab79808cccf7c2648c68beb8fd2b55d
Marianna Samylova <marianna.ask@gmail.com>
2018-10-28 23:32:52 +0100

    login tests added

--- pages/home.js
+++ pages/home.js
@@ -1,13 +1,14 @@
 let util = require('util');
 
-const elementFormat ='@%s';
+const elementFormat = '@%s';
 
 let menuCommands = {
     clickElementOnThePage: function (name) {
-        let element = util.format(elementFormat,name);
+        let element = util.format(elementFormat, name);
         this.click(element);
         return this;
-    }
+    },
+
 };
 
 module.exports = {
@@ -19,35 +20,43 @@ module.exports = {
             locateStrategy: 'xpath'
         },
         shortTermAccommodation: {
-            selector: '//a[contains(@href, "http://ubytovanie.sponadev.qity.sk/sresidence")]',
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/sresidence")]',
+            locateStrategy: 'xpath'
+        },
+        shortTermGateHouseAccommodation: {
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/gatehouse/sresidence")]',
             locateStrategy: 'xpath'
         },
         booking: {
-            selector: '//a[contains(@href, "http://ubytovanie.sponadev.qity.sk/webBooking")]',
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/webBooking")]',
             locateStrategy: 'xpath'
         },
         longTermAccommodation: {
-            selector: '//a[contains(@href, "http://ubytovanie.sponadev.qity.sk/lresidence")]',
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/lresidence")]',
             locateStrategy: 'xpath'
         },
-        statistics:
-            {
-                selector: "//a[contains(@href, 'http://ubytovanie.sponadev.qity.sk/statistic/sresidence')]",
-                locateStrategy: 'xpath'
-            },
-        calendar:
-            {
-                selector: '//a[contains(@href, "http://ubytovanie.sponadev.qity.sk/calendar")]',
-                locateStrategy: 'xpath'
-            },
-
-        createShortTermAccommodation: {
-            selector: '//a[contains(@href, "http://ubytovanie.sponadev.qity.sk/sresidence/create")]',
+        longTermGateHouseAccommodation: {
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/gatehouse/lresidence")]',
             locateStrategy: 'xpath'
-
         },
-        createLongTermAccommodation: {
-            selector: '//div/p/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/lresidence/create")]',
+        longTermAccommodationPayments: {
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/lresidencepayments/imports")]',
+            locateStrategy: 'xpath'
+        },
+        statistics: {
+            selector: "//li/a[contains(@href, 'http://ubytovanie.sponadev.qity.sk/statistic/sresidence')]",
+            locateStrategy: 'xpath'
+        },
+        calendar: {
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/calendar")]',
+            locateStrategy: 'xpath'
+        },
+        calendarGateHouse: {
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/gatehouse/calendar")]',
+            locateStrategy: 'xpath'
+        },
+        allDormitoriesList: {
+            selector: '//li/a[contains(@href, "http://ubytovanie.sponadev.qity.sk/servisZone")]',
             locateStrategy: 'xpath'
         }
     }
--- tests/logingroup/login_test.js
+++ tests/logingroup/login_test.js
@@ -1,28 +1,51 @@
 const config = require('../config.json');
 
 module.exports = {
-    tags: ['login','sanity'],
-    'Login as an admin' : function (client) {
-        const loginPage = client.page.login_spona();
+
+    tags: ['login', 'sanity'],
+    'Login as an admin': function (client) {
+        let loginPage = client.page.login_spona();
         loginPage.navigate();
-        loginPage.loginToSystem(config.admin.email,config.admin.password);
-        client.pause(2000);
+        loginPage.loginToSystem(config.admin.email, config.admin.password);
         let homePage = client.page.home();
         homePage.expect.element('@homeHeader').to.be.present.after(2000);
-        homePage.expect.element('@homeHeader').to.contain.text('Informačný systém pre ubytovanie');
+        homePage.expect.element('@longTermAccommodationPayments').to.be.present.after(2000);
+        homePage.expect.element('@allDormitoriesList').to.be.present.after(2000);
+        homePage.expect.element('@homeHeader').to.be.present.after(2000);
+        homePage.expect.element('@shortTermAccommodation').to.be.present.after(2000);
+        homePage.expect.element('@booking').to.be.present.after(2000);
+        homePage.expect.element('@longTermAccommodation').to.be.present.after(2000);
+        homePage.expect.element('@calendar').to.be.present.after(2000);
+        homePage.expect.element('@statistics').to.be.present.after(2000);
         client.end();
     },
 
-    'Login as a user' : function (client) {
-        const loginPage = client.page.loginSpona();
+    'Login as a user': function (client) {
+        let loginPage = client.page.login_spona();
         loginPage.navigate();
-        loginPage.loginToSystem(config.user.email,config.user.password);
-        client.pause(2000);
+        loginPage.loginToSystem(config.user.email, config.user.password);
         let homePage = client.page.home();
         homePage.expect.element('@homeHeader').to.be.present.after(2000);
-        homePage.expect.element('@homeHeader').to.contain.text('Informačný systém pre ubytovanie');
-
-
+        homePage.expect.element('@shortTermAccommodation').to.be.present.after(2000);
+        homePage.expect.element('@booking').to.be.present.after(2000);
+        homePage.expect.element('@longTermAccommodation').to.be.present.after(2000);
+        homePage.expect.element('@calendar').to.be.present.after(2000);
+        homePage.expect.element('@statistics').to.be.present.after(2000);
         client.end();
-    }
+    },
+
+    'Login as a doorman': function (client) {
+        let loginPage = client.page.login_spona();
+        loginPage.navigate();
+        loginPage.loginToSystem(config.doorman.email, config.doorman.password);
+        let homePage = client.page.home();
+        homePage.expect.element('@homeHeader').to.be.present.after(2000);
+        homePage.expect.element('@shortTermGateHouseAccommodation').to.be.present.after(2000);
+        homePage.expect.element('@longTermGateHouseAccommodation').to.be.present.after(2000);
+        homePage.expect.element('@calendarGateHouse').to.be.present.after(2000);
+        homePage.expect.element('@statistics').to.be.not.present.after(2000);
+        client.end();
+    },
+
+
 };
