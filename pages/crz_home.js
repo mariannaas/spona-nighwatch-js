e462f6997184e082b7aec74ab4a829dbc7ce94c7
Marianna Samylova <marianna.ask@gmail.com>
2018-10-28 23:42:47 +0100

    login tests fro CRZ user

---
+++ pages/crz_home.js
@@ -0,0 +1,12 @@
+let crzCommands = {}
+
+module.exports = {
+    url: 'http://ubytovanie.sponadev.qity.sk/elfinder#elf_fls2_Lw',
+    commands: [crzCommands],
+    elements: {
+        elFinder: {
+            selector: '//div[@id="elfinder"]',
+            locateStrategy: 'xpath'
+        }
+    }
+};
