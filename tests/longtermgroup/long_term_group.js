e40cb9bd6d4fb108134fe3e409c75ecafe07a096
Marianna Samylova <marianna.ask@gmail.com>
2018-11-10 20:39:10 +0100

    return spaces

--- tests/longtermgroup/long_term_group.js
+++ tests/longtermgroup/long_term_group.js
@@ -2,6 +2,7 @@ const config = require('../config.json');
 let createLongTermAccommodationPage;
 
 module.exports = {
+
     before(client) {
         const loginPage = client.page.login_spona();
         loginPage.navigate();
@@ -11,10 +12,12 @@ module.exports = {
         homePage.expect.element('@homeHeader').to.be.present.after(2000);
         homePage.expect.element('@homeHeader').to.contain.text('Informačný systém pre ubytovanie');
     },
+
     beforeEach(client) {
         createLongTermAccommodationPage = client.page.longterm_accomodation_create_for_person();
         createLongTermAccommodationPage.navigate();
-   },
+    },
+    
     after(client) {
         client.end();
     },
